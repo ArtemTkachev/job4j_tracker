5c74e2510ee19da367c7e653dcfa9bc0f22ddfc1
TkachevArtem <art599@yandex.ru>
2020-09-19 13:56:09 +0300

    6. Метод замены заявки. Tracker.replace [#355733]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -10,7 +10,7 @@ public class Tracker {
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index] != null && items[index].getId() == id) {
+            if (items[index].getId() == id) {
                 rsl = index;
                 break;
             }
@@ -47,7 +47,7 @@ public class Tracker {
         Item[] rsl = new Item[size];
         int sizeRsl = 0;
         for (int i = 0; i < size; i++) {
-            if (items[i] != null && items[i].getName().equals(name)) {
+            if (items[i].getName().equals(name)) {
                 rsl[sizeRsl++] = items[i];
             }
         }
