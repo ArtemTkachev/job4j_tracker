4dc13a3f6f1eefdcaec2610e9dc1a204204d210b
ArtemTkachev <art59926@gmail.com>
2022-08-08 21:59:40 +0300

    2.6. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -43,6 +43,15 @@ public class StartUI {
                 } else {
                     System.out.println("Item replacement error.");
                 }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("The item was deleted successfully.");
+                } else {
+                    System.out.println("Item deletion error");
+                }
             } else {
                 run = false;
             }
